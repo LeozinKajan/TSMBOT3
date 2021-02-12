4e7ac857a5b78df8c20c046a4570ba1b8ff1cb1e
LeozinKajan <78815843+LeozinKajan@users.noreply.github.com>
2021-02-12 01:29:32 +0000

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,190 @@
+const help = (prefix) => {
+	return `         *𝙏𝙎𝙈 𝘽𝙊𝙏*
+
+ INFO:✅
+
+ *DONO* : NABUTO LS
+ PARCEIRIAS: GUSTAVO, ITACHI DO HYPE
+
+ *WHATSAPPS DO PROPRIETÁRIO* :
+ 
+ *DONO* (𝙏𝙎𝙈 𝙆𝘼𝙅𝘼𝙉): wa.me/351965098553
+ 
+
+
+
+
+
+
+ 
+ *CANAIS NO YOUTUBE* :
+
+ *CANAL DO DONO* : https://www.youtube.com/channel/UCkyon7X9Q0BUfFvGLFnHFkw
+
+
+
+
+   [𝘾𝘼𝙎𝙊 𝘼𝙇𝙂𝙐𝙈 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝙉 𝙀𝙎𝙏𝙄𝙑𝙀𝙍 𝘿𝘼𝙉𝘿𝙊, 𝙈𝙀 𝘿𝙀𝙎𝘾𝙐𝙇𝙋𝘼 !]
+
+
+
+ 
+    𝑩𝑬𝑴 𝑽𝑰𝑵𝑫𝑶 𝑨𝑶 𝑴𝑬𝑵𝑼 𝑫𝑶 𝑩𝑶𝑻🤭
+
+
+
+
+    【 *MAIS USADOS RECENTEMENTE* 】
+  
+  ➡ ${prefix}sticker
+    【FAZER FIGURINHA】
+  ➡ ${prefix}toimg
+    【CONVERTE FIGURINHA EM IMAGEM】
+  ➡ ${prefix}tomp3
+    【PEGAR ÁUDIO DE ALGUM VÍDEO】
+
+ 
+
+     【FOTOS ANIME/LOLI e etc】
+ 
+
+  ➡${prefix}loli
+  【foto de loli】
+
+  ➡${prefix}nekoanime
+   【NEKO ANIME】
+
+  ➡${prefix}randomanime
+   【anime aleatório】
+
+
+  ➡${prefix}pokémon
+   【foto de pokémon】
+
+
+     
+
+
+        *𝙈𝙀𝙇𝙃𝙊𝙍𝙀𝙎 𝙎𝙊𝘽𝙍𝙀 𝘼́𝙐𝘿𝙄𝙊*
+ 
+  ➡ ${prefix}play nome da música
+    【DOWNLOAD MÚSICA】
+  ➡ ${prefix}tts pt texto
+    【FALAR SEU TEXTO】
+
+
+
+      +18 *NÃO USE NO GRUPO SUJEITO A BAN*
+
+  
+  ➡${prefix}hentai
+    【foto hentai】
+
+  ➡${prefix}randomhentaio
+    【random hentai】
+
+
+
+
+
+          【 *FONTES* 】
+
+  ➡ ${prefix}bpink
+  ➡ ${prefix}marvellogo
+  ➡ ${prefix}snowwrite
+  ➡ ${prefix}3dtext
+  ➡ ${prefix}ninjalogo
+  ➡ ${prefix}water
+  ➡ ${prefix}firetext
+  ➡ ${prefix}logowolf
+  ➡ ${prefix}logowolf2
+  ➡ ${prefix}phlogo
+  ➡ ${prefix}glitch        𝑨 𝑴𝑨𝑰𝑶𝑹𝑰𝑨 𝑵𝑨̃𝑶 𝑷𝑬𝑮𝑨         
+  ➡ ${prefix}neonlogo
+  ➡ ${prefix}neonlogo2
+  ➡ ${prefix}lionlogo
+  ➡ ${prefix}jokerlogo
+  ➡ ${prefix}shadow
+  ➡ ${prefix}burnpaper
+  ➡ ${prefix}coffee
+  ➡ ${prefix}lovepaper
+  ➡ ${prefix}woodblock
+  ➡ ${prefix}qowheart
+  ➡ ${prefix}mutgrass
+  ➡ ${prefix}undergocean
+  ➡ ${prefix}woodenboards
+  ➡ ${prefix}wolfmetal
+  ➡ ${prefix}metalictglow
+  ➡ ${prefix}8bit
+  ➡ ${prefix}ttp
+  ➡ ${prefix}herrypotter
+  ➡ ${prefix}pubglogo
+  ➡ ${prefix}quotemaker
+        
+       【 *MEDIA* 】
+  
+
+  ➡ ${prefix}trendtwit
+  ➡ ${prefix}randomkpop
+     【FOTOS ALEATÓRIA DE KPOP🤢】
+  ➡ ${prefix}ytsearch
+    【PESQUISA】
+      
+
+
+
+     【 *DOWNLOADER* 】
+  
+  ➡ ${prefix}images
+    【IMAGENS ALEATÓRIA】
+  ➡ ${prefix}ytmp3
+    【MP3】
+  ➡ ${prefix}ytmp4
+    【DOWNLOAD MP4(VIDEO)】
+  ➡ ${prefix}tiktok
+  ➡ ${prefix}joox
+         
+
+           *MEME*
+      
+
+
+  ➡  ${prefix}meme
+  ➡ 【MEME INDONÉSIO】
+  ➡  ${prefix}memeindo
+  ➡ 【MEMEINDO INDONESIO】
+ 
+
+
+
+
+       【 *STALK* 】
+  
+  
+  ➡ ${prefix}tiktokstalk
+     【stalkear tik tok】
+  ➡ ${prefix}igstalk
+
+
+
+
+
+          【 SÓ FUNCIONA COM O DONO 】
+
+
+  
+   ➭ ${prefix}setprefix
+     【MUDAR PREFIXO】
+   ➭ ${prefix}block
+     【BLOQUEAR】
+   ➭ ${prefix}bc
+     【TM】
+   ➭ ${prefix}bcgc
+     【TM SÓ PARA MEMBROS DO GP】
+   ➭ ${prefix}clearall
+     【APAGAR TODAS MSG】
+
+`
+}
+
+exports.help = help
